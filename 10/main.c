904fd245cdd9bd2cfdfe2f5809796e5c96f12ec8
mhw <mhw@ubuntu.(none)>
2017-07-20 06:08:21 -0700

    添加了第10课第四小节c与汇编汇合编程

--- 10/main.c
+++ 10/main.c
@@ -1,10 +1,25 @@
 #define GPKCON (volatile unsigned long*)0x7f008800
 #define GPKDAT (volatile unsigned long*)0x7f008808
-
-int gboot_main()
+void led_on1(void)
 {
     *(GPKCON) = 0x11110000;
     *(GPKDAT) = 0xa0;
+}
+void led_on2(void)
+{
+    __asm__(
+	"ldr r1, =0x11110000\n"
+	"str r1, [%0]\n"
+	"ldr r1, =0x00a0\n"
+	"str r1, [%1]\n"
+	:
+        :"r"(GPKCON),"r"(GPKDAT)
+	:"r1"
+         );
+}
+int gboot_main()
+{
+    led_on2();
     
     return 0;    
 }
